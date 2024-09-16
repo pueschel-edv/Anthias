33e4305b0fe2875ab2f5a62c4e3a024ff31f711f
pueschel-edv <pueschel@lernenfoerdern.de>
2024-09-16 08:01:47 +0200

    Add files via upload

---
+++ webview/src/view.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <QWebEngineView>
+#include <QWebEnginePage>
+#include <QWidget>
+#include <QEventLoop>
+#include <QNetworkReply>
+
+class View : public QWebEngineView
+{
+    Q_OBJECT
+
+public:
+    explicit View(QWidget* parent);
+
+    void loadPage(const QString &uri);
+    void loadImage(const QString &uri);
+
+private slots:
+    void handleAuthRequest(QNetworkReply*, QAuthenticator*);
+
+private:
+    QWebEnginePage* pre_loader;
+    QEventLoop pre_loader_loop;
+};
